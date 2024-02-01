46214a8a5cd1d7f66844307b5f2a9131c94dd851
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-02-01 17:08:19 +0100

    initialization index.ts backend

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+// import "reflect-metdata";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// import db from "./db";
+import { buildSchema } from "type-graphql";
+
+
+async function main() {
+    console.log("toto")
+    // const shema = await buildSchema({
+    //     resolvers: [],
+    // })
+
+    // const 
+}
+
+main()
