b31e1c85ceaa6eb5875acfdaf5035b2f48fcad40
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-03-13 17:48:51 +0100

    fix css global chakra

---
+++ frontend/src/styles/theme/global.ts
@@ -0,0 +1,15 @@
+const global =  {
+  // Styles for MainBox
+  ".main-box": {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  // Styles for FormBox
+  ".form-box": {
+    width: "500px",
+  },
+}
+
+export default global;
