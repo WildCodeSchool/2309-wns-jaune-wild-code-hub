caecc62aae72bca59613ccec845149854079827d
Jean-Marc Millet <millet.jm@live.com>
2024-04-17 17:27:59 +0200

    add type for flexDirection to avoid ts error

--- frontend/src/styles/theme/components/Box.ts
+++ frontend/src/styles/theme/components/Box.ts
@@ -1,20 +1,19 @@
-import { Container, background, defineStyleConfig } from "@chakra-ui/react";
-
-const box =  {
-    main: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-    },
-    containerBox : {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center" ,
-    },
-    form: {
-        width: "400px",
-    },
-}
+import { ResponsiveValue } from "@chakra-ui/react";
+const box = {
+  main: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  containerBox: {
+    display: "flex",
+    flexDirection: "column" as ResponsiveValue<"column" | "row">,
+    alignItems: "center",
+  },
+  form: {
+    width: "400px",
+  },
+};
 
 export default box;
