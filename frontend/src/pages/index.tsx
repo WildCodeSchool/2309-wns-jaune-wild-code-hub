41fedb1295030eb5c4d50e52f3ab8f374e59f448
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-04-18 15:47:26 +0200

    toto add

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home() {
         gap={"2rem"}
       >
         <Heading variant="homepage" textAlign="center">
-          Create, collaborate, make an impact
+          Create, collaborate, make an impact TOTO v2 
         </Heading>
         <Text
           color={"text"}
