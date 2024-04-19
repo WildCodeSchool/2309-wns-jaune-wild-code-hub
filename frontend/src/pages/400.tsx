1173060e8e5f78397850b00554119e4a2b1c2355
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-04-19 20:55:54 +0200

    update error 400 Page not found

--- frontend/src/pages/400.tsx
+++ frontend/src/pages/400.tsx
@@ -1,7 +1,8 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Button } from "@chakra-ui/react";
+import Link from "next/link";
 import components from "@/styles/theme/components";
 
-const Unauthorized = () => {
+const Error404 = () => {
   return (
     <Box
       {...components.Box.main}
@@ -10,12 +11,22 @@ const Unauthorized = () => {
       bgImage="url(/BGForm.png)"
     >
       <Box {...components.Box.containerBox} p={30}>
-        <Text fontSize="5xl" color="white" as="b">
-          ERROR 404 !
+        <Text fontSize="5xl" color="white" as="b" mb={10}>
+          ERROR 400 - Page Not Found
         </Text>
+        <Text fontSize="lg" color="white">
+          Oops! It seems like the page you re looking for does not exist.
+        </Text>
+        <Box mt={10}>
+          <Link  href="/" passHref>
+            <Button colorScheme="primary" variant="outline">
+              Go Back Home
+            </Button>
+          </Link>
+        </Box>
       </Box>
     </Box>
   );
 };
 
-export default Unauthorized;
+export default Error404;
