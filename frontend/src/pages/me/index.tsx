b75588e250ebd5cab2493c34f04f46ad063b8719
JeanMMillet <millet.jm@live.com>
2024-06-14 09:12:49 +0200

    test width layout

--- frontend/src/pages/me/index.tsx
+++ frontend/src/pages/me/index.tsx
@@ -1,12 +1,15 @@
-import Sidebar from "@/components/Sidebar";
 import SidebarLayout from "@/components/SidebarLayout";
 import { Flex } from "@chakra-ui/react";
+
 import React from "react";
 
 const Workspace = () => {
   return (
     <SidebarLayout>
-      <h1>Welcome to your Workspace</h1>
+      <Flex justify={"space-between"} width={"100%"} pr={"2rem"}>
+        <h1>Welcome to your Workspace</h1>
+        <h1>TEST</h1>
+      </Flex>
     </SidebarLayout>
   );
 };
