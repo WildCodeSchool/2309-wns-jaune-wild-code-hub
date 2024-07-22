dd076fecda89b42f0570b5d33193b7fa351b7a2e
JeanMMillet <millet.jm@live.com>
2024-07-22 12:28:30 +0200

    add number of characters in regex

--- frontend/src/regex.ts
+++ frontend/src/regex.ts
@@ -1,12 +1,13 @@
 // Auth
 export const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-export const pseudoRegex = /^[a-zA-Z0-9À-ÿ]+$/;
-export const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).*$/;
+export const pseudoRegex = /^[a-zA-Z0-9À-ÿ]{3,20}$/;
+export const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/;
 
 //Editor
+
 export const fileNamePattern = /^[^\\\/:*?"<>|]+\.(js|css|html)$/;
 
 //Check regex
-export const checkRegex = (regex : RegExp, str : string) => {
-    return regex.test(str)
-}
+export const checkRegex = (regex: RegExp, str: string) => {
+  return regex.test(str);
+};
