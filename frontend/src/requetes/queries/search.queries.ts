2af7df631e003b30eba206d7f72c2c2a970ba828
Milie971 <e.lolia971@gmail.com>
2024-07-24 19:12:09 -0400

    comment ignore searchbar

--- frontend/src/requetes/queries/search.queries.ts
+++ frontend/src/requetes/queries/search.queries.ts
@@ -1,23 +1,23 @@
-import { gql } from "@apollo/client";
-import { identity } from "lodash";
+// import { gql } from "@apollo/client";
+// import { identity } from "lodash";
 
-export const SEARCH_QUERY = gql`
-  query Search($term: String!) {
-    search(term: $term)
-    ... on File {
-      id
-      name
-      category
-    }
-    ... on User {
-      id
-      pseudo
-      email
-    }
-    ... on Project {
-      id
-      name
-      category
-    }
-  }
-`;
+// export const SEARCH_QUERY = gql`
+//   query Search($term: String!) {
+//     search(term: $term)
+//     ... on File {
+//       id
+//       name
+//       category
+//     }
+//     ... on User {
+//       id
+//       pseudo
+//       email
+//     }
+//     ... on Project {
+//       id
+//       name
+//       category
+//     }
+//   }
+// `;
