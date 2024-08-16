6ff727a7db43add1a1a72d6dcc2a986b32abc625
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-08-17 01:20:23 +0200

    fix code

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -19,18 +19,7 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
   const client = new ApolloClient({
-    // uri: "http://localhost:4000",
     uri: `${API_URL}`,
-    // link: new HttpLink({
-    //   uri:
-    //   typeof window !== "undefined" && location.origin.includes("localhost")
-    //     ? "http://localhost:4000"
-    //     : `${window.location.origin}/graphql`,
-    //   credentials: "include",
-    //   headers: {
-    //     "apollo-require-preflight": "true",
-    //   },
-    // }),
     cache: new InMemoryCache(),
     credentials: "include",
   });
